9666b0c1138df9c174bfb907236920e14909bc49
ASAPNYEL <danyelhenrique941@gmail.com>
2024-10-27 14:04:37 -0300

    Create Ex3.py

---
+++ Vetores/Ex3.py
@@ -0,0 +1,10 @@
+# Implemente um funcao que retorne o maior elemento de um
+# vetor de inteiro
+def maior_elemento(vetores):
+    maior = vetores[0]
+    for i in range(len(vetores)):
+        if vetores[i] > maior:
+            maior = vetores[i]
+    return maior
+vetores = [1,2,3,4,5,6,7,8,9,-9]
+print(maior_elemento(vetores))
