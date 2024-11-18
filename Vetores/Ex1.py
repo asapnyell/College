6e016bfbeed7174aa1d5daf1286a131e1fe553dc
ASAPNYEL <danyelhenrique941@gmail.com>
2024-11-18 12:38:44 -0300

    Update Ex1.py

--- Vetores/Ex1.py
+++ Vetores/Ex1.py
@@ -1,6 +1,7 @@
 meses = ['jan','fev','mar','abr','mai','jun','jul','ago','set','out','nov','dez']
 mes = int(input("Digite o numero do mes: "))
-if mes <1 or mes > 12:
-    print("Mes invalido")
+
+if mes <1 or mes >12:
+        print("Mes invalido")
 else:
     print(meses[mes - 1])
